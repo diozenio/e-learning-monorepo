39e6aac988d2948368444104d715b925cc70e736
diozenio <d.dionisiobsl@gmail.com>
2025-05-24 13:00:58 -0300

    feat: Implement login module

---
+++ apps/auth/src/modules/login/LoginUseCase.ts
@@ -0,0 +1,39 @@
+import { generateToken } from '@/lib/jwt/generateToken';
+import { comparePasswords } from '@/lib/security/password';
+import { IUserRepository } from '@/repositories/users';
+import { AppError } from '@/shared/http/errors/AppError';
+
+import { LoginParams } from './LoginParams';
+
+class LoginUseCase {
+  constructor(private userRepository: IUserRepository) {}
+
+  async execute({ email, password }: LoginParams) {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Invalid credentials', 401);
+    }
+
+    const passwordMatch = await comparePasswords(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Invalid credentials', 401);
+    }
+
+    const token = generateToken({
+      payload: { sub: user.id, email: user.email },
+    });
+
+    return {
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    };
+  }
+}
+
+export { LoginUseCase };
