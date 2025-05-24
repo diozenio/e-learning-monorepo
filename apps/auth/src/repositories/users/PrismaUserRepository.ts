5da3b5d51b7aeccae3677a6e3f3a4e0d16a4a5a6
diozenio <d.dionisiobsl@gmail.com>
2025-05-24 12:59:06 -0300

    feat: Implement users repository

---
+++ apps/auth/src/repositories/users/PrismaUserRepository.ts
@@ -0,0 +1,40 @@
+import { User } from '@prisma/client';
+
+import { prisma } from '@/lib/prisma';
+
+import { IUserRepository, UserModel } from './IUserRepository';
+
+export class PrismaUserRepository implements IUserRepository {
+  async findByEmail(email: string): Promise<UserModel | null> {
+    const userFromDb = await prisma.user.findUnique({ where: { email } });
+    if (!userFromDb) {
+      return null;
+    }
+    return this.toUser(userFromDb);
+  }
+
+  async create(userData: {
+    name: string;
+    email: string;
+    password: string;
+  }): Promise<UserModel> {
+    const newUserFromDb = await prisma.user.create({
+      data: {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      },
+    });
+
+    return this.toUser(newUserFromDb);
+  }
+
+  toUser(prismaUser: User): UserModel {
+    return {
+      id: prismaUser.id,
+      name: prismaUser.name,
+      email: prismaUser.email,
+      password: prismaUser.password,
+    };
+  }
+}
