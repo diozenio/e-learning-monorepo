41458cc166b4d9d5c5142da9add8b34bb12fe0b7
diozenio <d.dionisiobsl@gmail.com>
2025-06-09 19:41:45 -0300

    refactor: Move AuthProvider to private routes

--- apps/web/src/providers/AppProvider.tsx
+++ apps/web/src/providers/AppProvider.tsx
@@ -2,16 +2,8 @@
 
 import { PropsWithChildren } from 'react';
 
-import { AppLoadingProvider } from './AppLoadingContext';
-import { AuthProvider } from './AuthContext';
 import { QueryProvider } from './QueryProvider';
 
 export function AppProvider({ children }: PropsWithChildren) {
-  return (
-    <QueryProvider>
-      <AuthProvider>
-        <AppLoadingProvider>{children}</AppLoadingProvider>
-      </AuthProvider>
-    </QueryProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }
