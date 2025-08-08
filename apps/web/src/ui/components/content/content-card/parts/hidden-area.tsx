cfd573798826bff38c96cf90ca80855b759872f5
diozenio <d.dionisiobsl@gmail.com>
2025-08-08 15:15:57 -0300

    feat: Add accordion for module details

--- apps/web/src/ui/components/content/content-card/parts/hidden-area.tsx
+++ apps/web/src/ui/components/content/content-card/parts/hidden-area.tsx
@@ -1,7 +1,13 @@
 import { cva } from 'class-variance-authority';
-import { Clock } from 'lucide-react';
+import { BookOpen, Clock } from 'lucide-react';
 import Image from 'next/image';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/ui/primitives/accordion';
 import { Button } from '@/ui/primitives/button';
 import {
   Dialog,
@@ -13,6 +19,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/ui/primitives/dialog';
+import { Divider } from '@/ui/primitives/divider';
 import { Progress } from '@/ui/primitives/progress';
 
 import { ContentCardProps } from '../content-card.types';
@@ -62,7 +69,9 @@ function HiddenArea({
             <DifficultyInfo difficulty={difficulty} />
 
             <div className="text-muted-foreground ml-auto flex items-center gap-1.5 text-sm">
-              <span>{modules?.length} modules</span>
+              <span>
+                {modules?.length} {modules?.length === 1 ? 'module' : 'modules'}
+              </span>
             </div>
           </>
         )}
@@ -84,7 +93,10 @@ function HiddenArea({
               <DialogTrigger asChild>
                 <Button variant="outline">Read More</Button>
               </DialogTrigger>
-              <DialogContent className="gap-0 p-0" showCloseButton={false}>
+              <DialogContent
+                className="max-h-[95vh] gap-0 overflow-y-auto p-0"
+                showCloseButton={false}
+              >
                 <div className="bg-muted relative h-56 w-full overflow-hidden rounded-t-lg">
                   <Image
                     src={
@@ -95,12 +107,63 @@ function HiddenArea({
                     className="relative w-full object-cover"
                   />
                 </div>
-                <div className="w-full space-y-2 p-6">
+                <div className="w-full p-6">
                   <DialogHeader>
                     <DialogTitle>{title}</DialogTitle>
                     <DialogDescription>{description}</DialogDescription>
                   </DialogHeader>
-                  <DialogFooter className="sm:justify-between">
+                  <div className="mt-4 flex flex-col gap-1">
+                    <h3 className="text-sm font-semibold">Course Details</h3>
+                    <div className="flex w-full flex-row items-center gap-4">
+                      <div className="text-muted-foreground flex items-center gap-1.5 text-sm">
+                        <Clock size={14} />
+                        <span>{duration}H</span>
+                      </div>
+                      <DifficultyInfo difficulty={difficulty} />
+                      <div className="text-muted-foreground ml-auto flex items-center gap-1.5 text-sm">
+                        <span>
+                          {modules?.length}{' '}
+                          {modules?.length === 1 ? 'module' : 'modules'}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-4 space-y-2">
+                    <h3 className="text-sm font-semibold">Modules</h3>
+                    <div className="space-y-1.5">
+                      {modules &&
+                        modules.length > 0 &&
+                        modules?.map((module) => (
+                          <Accordion
+                            type="single"
+                            className="rounded border"
+                            collapsible
+                            key={module.id}
+                          >
+                            <AccordionItem value="item-1">
+                              <AccordionTrigger className="items-center px-3 py-2 hover:cursor-pointer">
+                                <div className="flex flex-row items-center gap-2">
+                                  <span>
+                                    <BookOpen className="size-4" />
+                                  </span>
+                                  <p>{module.title}</p>
+                                </div>
+                              </AccordionTrigger>
+                              <AccordionContent className="pb-2">
+                                <Divider />
+                                <ul className="mt-2 list-decimal space-y-1.5 px-8">
+                                  {module.lessons &&
+                                    module.lessons.map((lesson) => (
+                                      <li key={lesson.id}>{lesson.title}</li>
+                                    ))}
+                                </ul>
+                              </AccordionContent>
+                            </AccordionItem>
+                          </Accordion>
+                        ))}
+                    </div>
+                  </div>
+                  <DialogFooter className="mt-8 sm:justify-between">
                     <DialogClose asChild>
                       <Button type="button" variant="secondary">
                         Close
